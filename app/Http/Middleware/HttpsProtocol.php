280943e1f4342b11716518ec98d4841f04745d2d
Patrick Barbosa <patrickbarbosa.dp@gmail.com>
2023-02-02 04:17:07 -0300

    Resolvendo problema de cors e setando redirecionamento quando a rota nao for segura(https)

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HttpsProtocol
+{
+    private const APP_ENV_PRODUCTION = 'production';
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (
+            !$request->secure()
+            && env('APP_ENV') === self::APP_ENV_PRODUCTION
+        ) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
