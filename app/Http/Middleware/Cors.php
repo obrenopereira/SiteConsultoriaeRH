1f9e8a67cbc673586547f1b3b6a9d101138201a3
patrick <patrickbarbosa.dp@gmail.com>
2023-02-03 03:20:43 -0300

    Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,6 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', "*")
-            ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
-            ->header('Access-Control-Allow-Headers', "*");
+            ->header('Access-Control-Allow-Origin', "*");
     }
 }
