2b2d000803288ae737d2ede26f14dd770c5d1670
patrick <patrickbarbosa.dp@gmail.com>
2023-02-03 22:38:40 -0300

    Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,7 +15,12 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', "http://www.tsconsultoria.com.br,https://tsconsultoria.com.br,http://tsconsultoria.com.br/");
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin' , 'http://www.tsconsultoria.com.br,https://tsconsultoria.com.br,http://tsconsultoria.com.br/');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+
+       return $response;
     }
 }
