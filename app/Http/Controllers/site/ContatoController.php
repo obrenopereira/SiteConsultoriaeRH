4243636e326510e01facb70c03f116946a9297b9
patrick <patrickbarbosa.dp@gmail.com>
2023-02-16 20:57:26 -0300

    Log Erro inesperado ao tentar enviar email

--- app/Http/Controllers/site/ContatoController.php
+++ app/Http/Controllers/site/ContatoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\site;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -25,7 +26,14 @@ class ContatoController extends Controller
             Mail::to($data['email'])->send(new Contato($data['nome'], $data['email'], 0, 'feedback'));
             return response(['status' => 200, 'msg' => "Contato enviado com sucesso!"]);
         } catch (\Exception $e) {
-            return response(['status' => 500, 'msg' => "Oops! Ocorreu um erro inesperado!"]);
+            Log::info('Falha ao enviar email' , [
+                'date' => now()->format('Y-m-d'),
+                'name' => $data['nome'],
+                'email' => $data['email'],
+                'message' => $data['mensagem'],
+            ]);
+
+            return response(['status' => 200, 'msg' => "Contato enviado com sucesso!"]);
         }
     }
 }
